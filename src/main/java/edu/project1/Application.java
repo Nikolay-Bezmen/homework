5db716fb44ab5a3a3e9225609532c5d9ea285a3a
Nikolay Bezmen <nikbezm2004@gmail.com>
2023-10-20 01:51:30 +0300

    second.2 remark

--- src/main/java/edu/project1/Application.java
+++ src/main/java/edu/project1/Application.java
@@ -2,14 +2,13 @@ package edu.project1;
 
 import edu.project1.Hangman.Hangman;
 
-@SuppressWarnings("UncommentedMain")
-public class App {
+public class Application {
 
     public static void main(java.lang.String[] args) {
         Hangman hangman = new Hangman();
         hangman.run();
     }
 
-    private App() {
+    private Application() {
     }
 }
