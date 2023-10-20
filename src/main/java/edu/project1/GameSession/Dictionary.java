b7962efe3ae95e7617bbd36077ea80eabe174e2b
Nikolay Bezmen <nikbezm2004@gmail.com>
2023-10-20 22:56:34 +0300

    fourth remark

--- src/main/java/edu/project1/GameSession/Dictionary.java
+++ src/main/java/edu/project1/GameSession/Dictionary.java
@@ -3,15 +3,15 @@ package edu.project1.GameSession;
 import java.util.List;
 
 public class Dictionary {
-    private static final List<String> setWords = List.of("banana", "chocolate", "coffee", "apple", "horse");
+    private static final List<String> SET_WORD = List.of("banana", "chocolate", "coffee", "apple", "horse");
 
     public static String getRandomWord() {
-        int indexOfRandomAccess = (int) System.currentTimeMillis() % (setWords.size());
-        return setWords.get(indexOfRandomAccess);
+        int indexOfRandomAccess = (int) System.currentTimeMillis() % (SET_WORD.size());
+        return SET_WORD.get(indexOfRandomAccess);
     }
 
     public static List<String> getSetWords() {
-        return setWords;
+        return SET_WORD;
     }
 
     private Dictionary() {
