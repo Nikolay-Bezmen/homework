56b3a225405544336ef8302ee21fd3c70a44c136
Nikolay Bezmen <nikbezm2004@gmail.com>
2023-11-05 13:32:35 +0300

    new update main

--- src/main/java/edu/project1/GameSession/Dictionary.java
+++ src/main/java/edu/project1/GameSession/Dictionary.java
@@ -5,9 +5,10 @@ import java.util.Random;
 
 public class Dictionary {
     private static final List<String> SET_WORD = List.of("banana", "chocolate", "coffee", "apple", "horse");
-    private final static Random random = new Random();
+    private final static Random RANDOM = new Random();
+
     public static String getRandomWord() {
-        int indexOfRandomAccess = random.nextInt(SET_WORD.size());
+        int indexOfRandomAccess = RANDOM.nextInt(SET_WORD.size());
         return SET_WORD.get(indexOfRandomAccess);
     }
 
