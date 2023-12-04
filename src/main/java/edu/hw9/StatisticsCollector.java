1536a68913c19a8ac794c05ab13c09fdfc573e61
Nikolay Bezmen <nikbezm2004@gmail.com>
2023-12-05 01:39:28 +0300

    add task1 and tests for her

---
+++ src/main/java/edu/hw9/StatisticsCollector.java
@@ -0,0 +1,40 @@
+package edu.hw9;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class StatisticsCollector {
+    private final List<MetricStats> metrics = Collections.synchronizedList(new ArrayList<>());
+    private final ExecutorService executerStats;
+
+    public void push(String newMetric, double[] data) {
+        executerStats.execute(() -> {
+            double sum = 0;
+            double min = Double.MAX_VALUE;
+            double max = Double.MIN_VALUE;
+            for (double value : data) {
+                sum += value;
+                if (value < min) {
+                    min = value;
+                }
+                if (value > max) {
+                    max = value;
+                }
+            }
+            double average = sum / data.length;
+            metrics.add(new MetricStats(newMetric, sum, average, min, max));
+        });
+    }
+
+    public StatisticsCollector(int countThreads) {
+        executerStats = Executors.newFixedThreadPool(countThreads);
+    }
+
+    public List<MetricStats> getStats() {
+        return new ArrayList<>(metrics);
+    }
+}
+
