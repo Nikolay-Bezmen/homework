62af7656ccd820de05c6aa01cc162075a6c18c08
Nikolay Bezmen <nikbezm2004@gmail.com>
2023-12-07 00:23:26 +0300

    fix some error in build who linked with previos homeworks

--- src/main/java/edu/Project2/BFSSolver.java
+++ src/main/java/edu/Project2/BFSSolver.java
@@ -48,6 +48,7 @@ public class BFSSolver {
             int[] currDot = currPath.get(currPath.size() - 1);
             int x = currDot[0];
             int y = currDot[1];
+
             for (int[] pathDirection : PATH_DIRECTIONS) {
                 int dx = x + pathDirection[0];
                 int dy = y + pathDirection[1];
